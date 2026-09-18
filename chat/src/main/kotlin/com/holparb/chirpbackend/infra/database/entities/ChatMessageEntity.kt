e85c35137d777f31dc02772fb42adbf9bb1a4dda
Balazs Holpar <balazs02.holpar@gmail.com>
2026-09-18 12:52:57 +0200

    Always provide default chat message id to prevent StaleObjectException from JPA

--- chat/src/main/kotlin/com/holparb/chirpbackend/infra/database/entities/ChatMessageEntity.kt
+++ chat/src/main/kotlin/com/holparb/chirpbackend/infra/database/entities/ChatMessageEntity.kt
@@ -2,16 +2,7 @@ package com.holparb.chirpbackend.infra.database.entities
 
 import com.holparb.chirpbackend.domain.type.ChatId
 import com.holparb.chirpbackend.domain.type.ChatMessageId
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Index
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.OnDelete
 import org.hibernate.annotations.OnDeleteAction
@@ -30,7 +21,6 @@ import java.time.Instant
 )
 class ChatMessageEntity(
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     var id: ChatMessageId? = null,
     @Column(nullable = false)
     var content: String,
